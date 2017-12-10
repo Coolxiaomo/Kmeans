ff5633e10be8bdd4cc7ecd3d7506530c3d1f03d8
Coolxiaomo <wangmofei0420@gmail.com>
2017-12-10 22:58:59 +0800

    Upload code

---
+++ Kmeans.py
@@ -0,0 +1,103 @@
+import numpy as np
+import numpy.random as random
+from numpy import linalg as LA
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import spline
+
+class KMeans:
+    def __init__(self,filePath,data=None,xRow = 0,xCol = 0):
+        self.filePath = filePath
+        fr = open(filePath,'r+')
+        lines = fr.readlines()
+        retData = []
+        retCityName = []
+        for line in lines:
+            items = line.strip().split(",")
+            retCityName.append(items[0])
+            retData.append([float(items[i])  for i in range(1,len(items))])
+        self.dataName = retCityName
+        self.data = np.array(retData)
+        self.xRow, self.xCol = [i for i in self.data.shape]
+
+    def IntialCentroids(self,K):
+        randomX = random.permutation(self.data)
+        centroids = randomX[0:K, :]
+        idx = self.findClosetCentroids(centroids)
+        jSumMin = self.cosFunc(idx,centroids)
+        for i in range(1000):
+            randomX = random.permutation(self.data)
+            tempCentroids = randomX[0:K, :]
+            idx = self.findClosetCentroids(tempCentroids)
+            newCentroids = self.computeCentroids(idx,K)
+            jSum = self.cosFunc(idx,newCentroids)
+            if jSum < jSumMin:
+                centroids = newCentroids
+                jSumMin = jSum
+            else:
+                continue
+        return centroids
+
+    def cosFunc(self,idx,centroids):
+        jSum = 0
+        for i in range(self.xRow):
+            jSum = LA.norm(self.data[i,:] - centroids[int(idx[i]-1), :]) + jSum
+        jSum = jSum/(self.xRow)
+        return jSum
+
+    def findClosetCentroids(self, centroids):
+        kSize, kFeatures = [i for i in centroids.shape]
+        idx = np.zeros((self.xRow, 1))
+        for i in range(self.xRow):
+            idx[i] = 1
+            norm_min = LA.norm(self.data[i, :] - centroids[1, :])
+            for j in range(kSize):
+                if LA.norm(self.data[i, :] - centroids[j, :]) < norm_min:
+                    norm_min = LA.norm(self.data[i, :] - centroids[j, :])
+                    idx[i] = j
+        return idx
+
+    def computeCentroids(self,idx,K):
+        centroids_matrix = np.zeros((K,self.xRow))
+        centroids = np.zeros((K,self.xCol))
+        CK = np.zeros((K,1))
+        for i in range (self.xRow):
+            centroids_matrix[int(idx[i]),i] = 1
+            CK[int(idx[i])] = CK[int(idx[i])] + 1
+        for i in range(K):
+            centroids_Temp = np.dot(centroids_matrix,self.data)
+            centroids[i,:] = centroids_Temp[i,:]/CK[i,:]
+        return centroids
+
+    def jCostPlot(self,kNum):
+        x_data_plot = []
+        y_data_plot = []
+        for i in range(kNum):
+            x_data_plot.append(i + 2)
+            centroids = U.IntialCentroids(i + 2)
+            for j in range(300):
+                idx = U.findClosetCentroids(centroids)
+                centroids = U.computeCentroids(idx, i + 2)
+                jCos = U.cosFunc(idx, centroids)
+            y_data_plot.append(jCos)
+        x_data = np.array(x_data_plot)
+        y_data = np.array(y_data_plot)
+        xNew = np.linspace(x_data.min(), x_data.max(), 300)
+        ySmooth = spline(x_data, y_data, xNew)
+        plt.plot(xNew, ySmooth)
+        plt.show()
+        return None
+
+if __name__=='__main__':
+    U = KMeans('city.txt')
+    centroids = U.IntialCentroids(4)
+    for i in range(300):
+        idx = U.findClosetCentroids(centroids)
+        centroids = U.computeCentroids(idx, 4)
+    labeled_City = [[], [], [], []]
+    for i in range(len(U.dataName)):
+        labeled_City[int(idx[i])].append([U.dataName[i]])
+    for i in range(4):
+        print(labeled_City[i])
+    #U.jCostPlot(10)
+
